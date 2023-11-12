7e6fdaa5cddd7e7bef4bfea8a6701b2999fdd86b
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-11-12 18:21:48 +0200

    About page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
-import { Route, Routes } from 'react-router-dom';
+import About from './components/About/About';
+
+import './App.css';
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
         
         <Routes>
             <Route path='/' element={<Home />}></Route>
+            <Route path='/about' element={<About />}></Route>
         </Routes>
 
         <Footer />
