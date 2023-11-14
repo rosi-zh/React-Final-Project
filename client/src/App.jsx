31de4cfababa7bc11ebfea3e0333372895f1f4f2
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-11-14 16:47:57 +0200

    Add register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contacts from './components/Contacts/Contacts';
 import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path='/about' element={<About />}></Route>
             <Route path='/contacts' element={<Contacts />}></Route>
             <Route path='/login' element={<Login />}></Route>
+            <Route path='/register' element={<Register />}></Route>
         </Routes>
 
         <Footer />
