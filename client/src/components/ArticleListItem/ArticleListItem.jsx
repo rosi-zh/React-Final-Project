ba5463587ae80c47a9b6faa1cb897a3d0756886a
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-12-03 19:31:59 +0200

    Change linl

--- client/src/components/ArticleListItem/ArticleListItem.jsx
+++ client/src/components/ArticleListItem/ArticleListItem.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import Path from "../../utils/paths";
+
 export default function ArticleListItem({
     _id,
     title,
@@ -10,7 +13,7 @@ export default function ArticleListItem({
         <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
             <img className="img-fluid" src={imageUrl} alt={title} />
             <div className="bg-light p-4">
-                <a className="d-block h5 lh-base mb-4" href="">{title}</a>
+                <Link to={`${Path.Articles}/${_id}`} className="d-block h5 lh-base mb-4">{title}</Link>
                 <div className="text-muted border-top pt-4">
                     <small className="me-3"><i className="fa fa-user text-primary me-2"></i>{_ownerId}</small>
                     <small className="me-3"><i className="fa fa-calendar text-primary me-2"></i>{_createdOn}</small>
