e5f7b8a08c9858e3fdfaa061af133961ccdfa03a
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-12-03 21:06:02 +0200

    Update data service

--- client/src/components/ArticleListItem/ArticleListItem.jsx
+++ client/src/components/ArticleListItem/ArticleListItem.jsx
@@ -20,5 +20,5 @@ export default function ArticleListItem({
                 </div>
             </div>
         </div>
-);
+    );
 }
--- client/src/services/dataService.js
+++ client/src/services/dataService.js
@@ -2,13 +2,26 @@ import { get, post } from "./api";
 
 const endpoints = {
     all: '/data/articles',
-    create: '/data/articles'
+    create: '/data/articles',
+    byId: '/data/articles/'
 }
 
 export async function getAll() {
     return get(endpoints.all);
 }
 
+export async function getById(id) {
+    return get(endpoints.byId + id);
+}
+
 export async function create(articleData) {
     return post(endpoints.create, articleData);
+}
+
+export async function edit(id, articleData) {
+    return put(endpoints.byId + id, articleData);
+}
+
+export async function remove(id) {
+    return del(endpoints.byId + id);
 }
