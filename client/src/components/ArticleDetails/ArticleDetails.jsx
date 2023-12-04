10511bb60a2dabd8b31f034d4128b20fd87c566c
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-12-04 19:44:30 +0200

    Add Loader

--- client/src/components/ArticleDetails/ArticleDetails.jsx
+++ client/src/components/ArticleDetails/ArticleDetails.jsx
@@ -7,6 +7,7 @@ import PageTop from "../PageTop/PageTop";
 import AuthContext from "../../contexts/authContext";
 import Path from "../../utils/paths";
 import useAsync from "../../hooks/useAsync";
+import Loader from "../Loader/Loader";
 
 export default function ArticleDetails() {
     const navigate = useNavigate();
@@ -39,28 +40,33 @@ export default function ArticleDetails() {
             <PageTop title="Article" />
 
             <div className="container-xxl py-5">
-                <div className="container">
-                    <div className="row g-5">
-                        <div className="col-lg-5 wow fadeIn" data-wow-delay="0.1s">
-                            <img className="img-fluid" src={value.imageUrl} alt={value.title} />
-                        </div>
-                        <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-                            <div className="section-title">
-                                <p className="fs-5 fw-medium fst-italic text-primary">Featured Acticle</p>
-                                <h2 className="display-6">{value.title}</h2>
+                {loading 
+                    ? <Loader /> 
+                    : (
+                        <div className="container">
+                            <div className="row g-5">
+                                <div className="col-lg-5 wow fadeIn" data-wow-delay="0.1s">
+                                    <img className="img-fluid" src={value.imageUrl} alt={value.title} />
+                                </div>
+                                <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                                    <div className="section-title">
+                                        <p className="fs-5 fw-medium fst-italic text-primary">Featured Acticle</p>
+                                        <h2 className="display-6">{value.title}</h2>
+                                    </div>
+                                    <p className="mb-4">{value.text}</p>
+                                    {userId === value._ownerId && (
+                                        <>
+                                            <Link to={`/articles/${articleId}/edit`} className="btn btn-primary rounded-pill py-3 px-5 me-3">Edit</Link>
+                                            <button onClick={deleteButtonHandler} className="btn btn-primary rounded-pill py-3 px-5">Delete</button>
+                                        </>
+                                    )}
+                                </div>
                             </div>
-                            <p className="mb-4">{value.text}</p>
-                            {userId === value._ownerId && (
-                                <>
-                                    <Link to={`/articles/${articleId}/edit`} className="btn btn-primary rounded-pill py-3 px-5 me-3">Edit</Link>
-                                    <button onClick={deleteButtonHandler} className="btn btn-primary rounded-pill py-3 px-5">Delete</button>
-                                </>
-                            )}
                         </div>
-                    </div>
-                </div>
-            </div>
+                    )
+                }
 
+            </div>
         </>
     );
 }
