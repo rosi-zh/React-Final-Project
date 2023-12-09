967f32fdf68f7d01d0fd3c9c22741687549e769e
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-12-09 11:37:46 +0200

    Update ArticleListItem css

--- client/src/components/ArticleListItem/ArticleListItem.jsx
+++ client/src/components/ArticleListItem/ArticleListItem.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./ArticleListItem.css";
 import Path from "../../utils/paths";
 import convertDate from "../../utils/convertDate";
 
@@ -11,7 +12,9 @@ export default function ArticleListItem({
 
     return (
         <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-            <img className="img-fluid" src={imageUrl} alt={title} />
+            <div className="img-container">
+                <img className="img-fluid article-img" src={imageUrl} alt={title} />
+            </div>
             <div className="bg-light p-4">
                 <Link to={`${Path.Articles}/${_id}`} className="d-block h5 lh-base mb-4">{title}</Link>
                 <div className="text-muted border-top pt-4">
